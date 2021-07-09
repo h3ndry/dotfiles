32673f2d1f7ed75df932881a8482890f41aaa8c1
Simanga Hendry Khoza <h3ndry@users.noreply.github.com>
2021-07-09 13:26:49 +0000

    add: script to quick open folder in vim

---
+++ .local/bin/quickmove.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+items=`find ~/workspace -maxdepth 1 -mindepth 1 -type d`
+items+=`find ~/playround -maxdepth 1 -mindepth 1 -type d`
+items+=`find ~/.config -maxdepth 1 -mindepth 1 -type d`
+items+=`find ~ -maxdepth 1 -mindepth 1 -type d`
+
+echo "$items" | fzf | xargs -I {} nvim {}
