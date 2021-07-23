aa9a3add56b1a10a7eff3aa4c27ac7332ada4f79
Simanga Hendry Khoza <h3ndry@users.noreply.github.com>
2021-07-23 18:21:58 +0000

    list path in dmenu instead

--- .local/bin/quickmove.sh
+++ .local/bin/quickmove.sh
@@ -3,6 +3,7 @@
 items=`find ~/workspace -maxdepth 1 -mindepth 1 -type d`
 items+=`find ~/playround -maxdepth 1 -mindepth 1 -type d`
 items+=`find ~/.config -maxdepth 1 -mindepth 1 -type d`
-items+=`find ~ -maxdepth 1 -mindepth 1 -type d`
+# items+=`find ~ -maxdepth 1 -mindepth 1 -type d`
 
-echo "$items" | fzf | xargs -I {} nvim {} --cmd 'cd {}'
+# echo "$items" | fzf | xargs -I {} nvim {} --cmd 'cd {}'
+echo "$items" | dmenu | xargs -I {} st -e nvim {} --cmd 'cd {}'
