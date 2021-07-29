80f220f64976f44682057cfc9cef11f87545c50d
Hendry <h3ndry@users.noreply.github.com>
2021-07-29 11:33:51 +0000

    auto swith to first tag

--- .local/bin/quickmove.sh
+++ .local/bin/quickmove.sh
@@ -5,5 +5,11 @@ items+=`find ~/playround -maxdepth 1 -mindepth 1 -type d`
 items+=`find ~/.config -maxdepth 1 -mindepth 1 -type d`
 # items+=`find ~ -maxdepth 1 -mindepth 1 -type d`
 
+FOLDER=`echo "$items" | dmenu`
+
+if [ -d ${FOLDER} ]; then
+    xdotool key Super_L+1
+    st -e nvim ${FOLDER} --cmd cd ${FOLDER}
+fi
+
 # echo "$items" | fzf | xargs -I {} nvim {} --cmd 'cd {}'
-echo "$items" | dmenu | xargs -I {} st -e nvim {} --cmd 'cd {}'
