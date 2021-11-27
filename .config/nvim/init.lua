03f3024a81ca7f39c2c856a627bdd10831c67ac7
Hendry <h3ndry@users.noreply.github.com>
2021-11-27 09:41:04 +0000

    working

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,81 +1,40 @@
 -- It all start here
 require("packer").startup(
   function(use)
-    -- Packer can manage itself
     use "wbthomason/packer.nvim"
-    -- use "tpope/vim-commentary"
-    use {
-      "numToStr/Comment.nvim",
-      config = function()
-        require("Comment").setup()
-      end
-    }
-
-    -- use "nvim-telescope/telescope-fzy-native.nvim"
-
+    use "numToStr/Comment.nvim"
     use "h3ndry/ReplaceWithRegister"
     use "tpope/vim-repeat"
-
-    -- use 'edluffy/specs.nvim'
-
     use "cohama/lexima.vim"
-
+    use "chentau/marks.nvim"
     use "tpope/vim-surround"
     use "h3ndry/tokyonight.nvim"
-    use "SirVer/ultisnips"
-    use "honza/vim-snippets"
     use "neovim/nvim-lspconfig"
     use "ggandor/lightspeed.nvim"
-
     use "andymass/vim-matchup"
     use "tpope/vim-capslock"
     use "nathom/filetype.nvim"
-
     use {"nvim-telescope/telescope-fzf-native.nvim", run = "make"}
-
-    use {
-      "nvim-telescope/telescope.nvim",
-      requires = {{"nvim-lua/plenary.nvim"}}
-    }
-    -- use "tversteeg/registers.nvim"
-
+    use "nvim-lua/plenary.nvim"
+    use "nvim-telescope/telescope.nvim"
+    use "L3MON4D3/LuaSnip"
+    use "saadparwaiz1/cmp_luasnip"
+    use "rafamadriz/friendly-snippets"
+    use "saadparwaiz1/cmp_luasnip"
     use "hrsh7th/nvim-cmp"
     use "hrsh7th/cmp-buffer"
     use "hrsh7th/cmp-path"
     use "hrsh7th/cmp-nvim-lsp"
-    use "saadparwaiz1/cmp_luasnip"
     use "onsails/lspkind-nvim"
-
-    use "rafamadriz/friendly-snippets"
-    use "L3MON4D3/LuaSnip" -- Snippets plugin use "hrsh7th/vim-vsnip"
-    use "hrsh7th/vim-vsnip-integ"
-    use "hrsh7th/vim-vsnip"
-
-    -- Install nvim-cmp, and buffer source as a dependency
-
     use "golang/vscode-go"
-
-    -- use "kabouzeid/nvim-lspinstall"
-
-    use "williamboman/nvim-lsp-installer"
-
     use "nvim-lua/lsp_extensions.nvim"
     use "OrangeT/vim-csharp"
-
-    use {
-      "nvim-treesitter/nvim-treesitter",
-      branch = "0.5-compat"
-    }
-
-    use {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      branch = "0.5-compat",
-      run = ":TSUpdate"
-    }
-
+    use { "nvim-treesitter/nvim-treesitter", branch = "0.5-compat" }
+    use { "nvim-treesitter/nvim-treesitter-textobjects", branch = "0.5-compat", run = ":TSUpdate" }
     use "mhartington/formatter.nvim"
-
     use "norcalli/nvim-colorizer.lua"
+    use "lewis6991/gitsigns.nvim"
+    use "nvim-lua/plenary.nvim"
     use {
       "prettier/vim-prettier",
       ft = {
@@ -95,140 +54,32 @@ require("packer").startup(
       },
       run = "yarn install"
     }
+    use "ray-x/cmp-treesitter"
+    use "hrsh7th/cmp-cmdline"
+    use "hrsh7th/cmp-calc"
+    use "f3fora/cmp-spell"
 
-    use {
-      "glacambre/firenvim",
-      run = function()
-        vim.fn["firenvim#install"](0)
-      end
-    }
 
-    use {
-      "lewis6991/gitsigns.nvim",
-      requires = {
-        "nvim-lua/plenary.nvim"
-      }
-    }
-    -- use 'sunjon/shade.nvim'
   end
 )
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.documentationFormat = {"markdown", "plaintext"}
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.preselectSupport = true
-capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-capabilities.textDocument.completion.completionItem.tagSupport = {valueSet = {1}}
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    "documentation",
-    "detail",
-    "additionalTextEdits"
+require("Comment").setup()
+
+
+local nvim_lsp = require("lspconfig")
+local servers = {"clangd", "rust_analyzer", "pyright", "tsserver", "html", "cssls", "emmet_ls"}
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities
   }
-}
+end
 
--- LSP language_server
-local bin_name =
-  "/home/hendry/.local/share/nvim/lspinstall/html/vscode-html/html-language-features/server/dist/node/htmlServerMain.js"
-local pid = vim.fn.getpid()
-local omnisharp_bin = "/home/hendry/.local/share/nvim/lspinstall/csharp/omnisharp/run"
-local sumneko_root_path = vim.fn.stdpath("cache") .. "/lspconfig/sumneko_lua/lua-language-server"
-local sumneko_binary = "/home/hendry/.local/share/nvim/lspinstall/lua/sumneko-lua-language-server"
-local runtime_path = vim.split(package.path, ";")
-local svelete_bin = "/home/hendry/.local/share/nvim/lspinstall/svelte/node_modules/svelte-language-server/bin/server.js"
-local css_path =
-  "/home/hendry/.local/share/nvim/lspinstall/css/vscode-css/css-language-features/server/dist/node/cssServerMain.js"
-local emmet_ls = "/home/hendry/.local/share/nvim/lsp_servers/emmet_ls/node_modules/emmet-ls/out/server.js"
 
-require "lspconfig".clangd.setup {
-  cmd = {"clangd", "--background-index"}
-}
-
-require "lspconfig".html.setup {
-  cmd = {"node", bin_name, "--stdio"},
-  capabilities = capabilities
-}
-
-require "lspconfig".emmet_ls.setup {
-  cmd = {"node", emmet_ls, "--stdio"},
-  filetypes = {"html", "css", "svelte", "jsx", "tsx"},
-  capabilities = capabilities
-}
-
-require "lspconfig".cssls.setup {
-  cmd = {"node", css_path, "--stdio"},
-  capabilities = capabilities
-}
-
-require "lspconfig".omnisharp.setup {
-  cmd = {omnisharp_bin, "--languageserver", "--hostPID", tostring(pid)}
-}
-
-require "lspconfig".rust_analyzer.setup {
-  cmd = {"rust_analyzer"}
-}
-
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-require "lspconfig".sumneko_lua.setup {
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
-        -- Setup your lua path
-        path = runtime_path
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {"vim"}
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true)
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false
-      }
-    }
-  }
-}
+require "luasnip".config.setup {}
+require "marks".setup {}
 
 require("telescope").load_extension("fzf")
-
--- require('neoclip').setup({
---       history = 1000,
---       enable_persistant_history = false,
---       db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
---       filter = nil,
---       preview = true,
---       default_register = '"',
---       content_spec_column = false,
---       on_paste = {
---         set_reg = false,
---       },
---       keys = {
---         i = {
---           select = '<cr>',
---           paste = '<c-p>',
---           paste_behind = '<c-k>',
---           custom = {},
---         },
---         n = {
---           select = '<cr>',
---           paste = 'p',
---           paste_behind = 'P',
---           custom = {},
---         },
---       },
---     })
 
 require("telescope").setup {
   defaults = {
@@ -248,23 +99,6 @@ require("telescope").setup {
 }
 
 -- require('telescope').load_extension('fzy_native')
-
-local lsp_installer = require("nvim-lsp-installer")
-
-lsp_installer.on_server_ready(
-  function(server)
-    local opts = {}
-
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-
-    -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
-    server:setup(opts)
-    vim.cmd [[ do User LspAttachBuffers ]]
-  end
-)
 
 -- require'shade'.setup({
 --   overlay_opacity = 50,
@@ -311,30 +145,6 @@ require("lspkind").init(
     }
   }
 )
-
--- okay this seems like a good plugin
-require "lspconfig".svelte.setup {
-  cmd = {"node", svelete_bin, "--stdio"}
-}
-
-require "lspconfig".texlab.setup {
-  cmd = {"/home/hendry/.local/share/nvim/lspinstall/latex/texlab"}
-}
-require "lspconfig".tsserver.setup {}
-
-require "lspconfig".phpactor.setup {
-  cmd = {"/home/hendry/.local/share/nvim/lspinstall/phpactor/bin/phpactor", "language-server"}
-}
-
-local nvim_lsp = require("lspconfig")
-local servers = {"clangd", "rust_analyzer", "pyright", "tsserver"}
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    -- on_attach = my_custom_on_attach,
-    capabilities = capabilities
-  }
-end
-
 -- Highlight on yank
 vim.api.nvim_exec(
   [[
@@ -420,8 +230,6 @@ vim.api.nvim_exec(
       exec "normal `k"
     endfunction
 
-    nnoremap <leader>rc :call Exec_on_term("normal")<CR>
-    vnoremap <leader>rc :<c-u>call Exec_on_term("visual")<CR>
 
 ]],
   false
@@ -441,7 +249,7 @@ vim.wo.relativenumber = true
 vim.o.hidden = true
 vim.o.breakindent = true
 vim.o.wrap = false
-vim.cmd [[set undofile]]
+-- vim.cmd [[set undofile]]
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.scrolloff = 1
@@ -449,21 +257,20 @@ vim.o.updatetime = 250
 vim.wo.signcolumn = "yes"
 
 -- Plugings Configuration
--- Setting that change How an lets see
--- autocmd VimResized * wincmd =
--- luasnip setup
-local luasnip = require("luasnip")
+-- local luasnip = require("luasnip")
 local cmp = require "cmp"
 local lspkind = require("lspkind")
 
 -- luasnip.stup{}
 -- nvim-cmp setup
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end
-  },
+    snippet = {
+      expand = function(args)
+        require'luasnip'.lsp_expand(args.body)
+      end
+    },
+
+
   mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
@@ -475,33 +282,17 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
     }
-
-    -- ["<Tab>"] = function(fallback)
-    --   if vim.fn.pumvisible() == 1 then
-    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
-    --   elseif luasnip.expand_or_jumpable() then
-    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-    --   else
-    --     fallback()
-    --   end
-    -- end,
-    -- ["<S-Tab>"] = function(fallback)
-    --   if vim.fn.pumvisible() == 1 then
-    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
-    --   elseif luasnip.jumpable(-1) then
-    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-    --   else
-    --     fallback()
-    --   end
-    -- end
-    --
   },
   sources = {
     -- {name = "gh_issues"},
-    {name = "nvim_lua"},
     {name = "nvim_lsp"},
-    {name = "path"},
     {name = "luasnip"},
+    {name = "nvim_lua"},
+    {name = "cmp-cmdline"},
+    {name = "cmp-treesitter"},
+    {name = "cmp-calc"},
+    {name = "cmp-spell"},
+    {name = "path"},
     {name = "buffer"}
   },
   experimental = {
@@ -511,15 +302,39 @@ cmp.setup {
     format = lspkind.cmp_format {
       with_text = true,
       menu = {
-        buffer = "[buff]",
         nvim_lsp = "[LSP]",
-        nvim_lua = "[api]",
-        path = "[path]",
-        luasnip = "[snip]"
+        nvim_lua = "[API]",
+        calc = "[CALC]",
+        cmdline = "[CMD]",
+        spell = "[SPELL]",
+        path = "[PATH]",
+        luasnip = "[SNIP]",
+        buffer = "[BUFF]",
       }
     }
   }
 }
+
+  cmp.setup.cmdline('?', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
 
 require("nvim-treesitter.configs").setup {
   textobjects = {
@@ -693,35 +508,11 @@ require("Comment").setup()
 
 require("nvim-treesitter.configs").setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  -- ignore_install = { "javascript" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    -- disable = { "c", "rust" },  -- list of language that will be disabled
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false
   }
 }
-
--- require('specs').setup{
---     show_jumps  = true,
---     min_jump = 30,
---     popup = {
---         delay_ms = 0, -- delay before popup displays
---         inc_ms = 10, -- time increments used for fade/resize effects
---         blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
---         width = 10,
---         winhl = "PMenu",
---         fader = require('specs').linear_fader,
---         resizer = require('specs').shrink_resizer
---     },
---     ignore_filetypes = {},
---     ignore_buftypes = {
---         nofile = true,
---     },
--- }
 
 require("gitsigns").setup {
   signs = {
@@ -860,5 +651,10 @@ vim.api.nvim_set_keymap("n", "<leader>_", ":res<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>|", ":vert res<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>w", "<C-w>", opts)
 
+vim.api.nvim_set_keymap("n", "<leader>R", ':call Exec_on_term("normal")<CR>', opts)
+vim.api.nvim_set_keymap("n", "<leader>R", ':<c-u>call Exec_on_term("visual")<CR>', opts)
+
 -- Random
 vim.api.nvim_set_keymap("n", "<leader>;", ":", opts)
+
+require'snippets'
